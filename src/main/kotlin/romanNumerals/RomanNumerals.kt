f3d5827e5ba9de5daf27319962e925ae56294bf5
Mario Russo <mario.russo@lastminute.com>
2019-12-11 00:29:23 +0100

    using junit 5 and fix missing cases

--- src/main/kotlin/romanNumerals/RomanNumerals.kt
+++ src/main/kotlin/romanNumerals/RomanNumerals.kt
@@ -1,30 +1,34 @@
 package romanNumerals
 
 
+val symbolsMap = listOf(
+    "IIIII" to "V",
+    "VV"    to "X",
+    "XXXXX" to "L",
+    "LL"    to "C",
+    "CCCCC" to "D",
+    "DD"    to "M",
+
+    "DCCCC" to "CM",
+    "CCCC"  to "CD",
+    "LXXXX" to "XC",
+    "XXXX"  to "XL",
+    "VIIII" to "IX",
+    "IIII"  to "IV"
+)
+
 fun decimalToRoman(number: Int): String = (
 
-            replicate("I")
-        `|` replace("IIIII" , "V")
-        `|` replace("VV"    , "X")
-        `|` replace("XXXXX" , "L")
-        `|` replace("LL"    , "C")
-        `|` replace("CCCCC" , "D")
-        `|` replace("DD"    , "M")
-
-        `|` replace("DCCCC" , "CM")
-        `|` replace("CCCC" , "CD")
-        `|` replace("LXXXX"  , "XC")
-        `|` replace("XXXX"  , "XL")
-        `|` replace("VIIII" , "IX")
-        `|` replace("IIII"  , "IV")
-
+             replicate("I")
+         `|` replaceSymbols(symbolsMap)
     )(number)
+
+
+fun replaceSymbols(symbols: List<Pair<String,String>>): (String) -> String =
+{  s: String -> symbols.fold(s, { acc,symbol -> acc.replace(symbol.first,symbol.second) }) }
 
 fun replicate(symbol: String): (Int) -> String =
     { number: Int -> IntRange(1, number).map { n -> symbol }.joinToString(separator = "") }
-
-fun replace(toBeReplaced: String, replacement: String) =
-    { s: String -> s.replace(toBeReplaced, replacement) }
 
 
 
